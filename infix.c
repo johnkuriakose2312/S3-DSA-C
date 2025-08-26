18f7f5973297a9195de429c28f65d750da62246e
John Kuriakose <johnkuriakose4421@gmail.com>
2025-08-26 13:57:01 +0530

    Add files via upload

---
+++ infix.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+
+int precedence(char op) {
+    if (op == '^') {
+        return 3;
+    }
+    else if (op == '*' || op == '/') {
+        return 2;
+    }
+    else if (op == '+' || op == '-') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    char infix[100], postfix[100], stack[100];
+    int top = -1, i = 0, j = 0;
+    char ch;
+
+    printf("Enter the infix expression: ");
+    scanf("%s", infix);
+
+    while (infix[i] != '\0') {
+        ch = infix[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+            postfix[j++] = ch;
+        }
+        
+        else if (ch == '(') {
+            stack[++top] = ch;
+        }
+ 
+        else if (ch == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = stack[top--];
+            }
+            top--; 
+        }
+        
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(ch)) {
+                postfix[j++] = stack[top--];
+            }
+            stack[++top] = ch;
+        }
+
+        i++;
+    }
+
+    
+    while (top != -1) {
+        postfix[j++] = stack[top--];
+    }
+
+    postfix[j] = '\0';
+
+    printf("The corresponding postfix expression is: %s\n", postfix);
+
+    return 0;
+}
+
+
